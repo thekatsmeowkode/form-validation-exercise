ddd043b1a09101d1bd747ca6e5238ebdef371ab8
Kat M <kmunson13@gmail.com>
2022-11-09 16:26:39 -0600

    Add form validation password input

--- script.js
+++ script.js
@@ -3,43 +3,62 @@ const email = document.getElementById('email')
 const zip = document.getElementById('zip')
 const password = document.getElementById('password')
 const passConfirm = document.getElementById('password2')
+const passRules = document.getElementsByClassName('pass-rules')
 
 email.addEventListener('blur', () => {validateEmailInput()})
 zip.addEventListener('blur', () => {validateZipInput()})
+// password.addEventListener('blur', () => {validatePassword()})
+password.addEventListener('input', (e) => {checkRules(e)})
+
+function checkRules(e) {
+    const rule1 = document.getElementById('rule1')
+    const rule2 = document.getElementById('rule2')
+    const rule3 = document.getElementById('rule3')
+    let password = e.target.value
+    let passwordElement = document.getElementById('password')
+    let passConfirm = document.getElementById('password2')
+    for (let i=0; i < passRules.length; i++) {
+        passRules[i].style.color = 'red'
+    }
+    if (password.length > 8) {rule1.style.color = 'green'}
+    if (/\d/.test(password)) {rule2.style.color = 'green'}
+    if (password === passConfirm.value) {rule3.style.color = 'green'}
+    if ((password.length>8) && (/\d/.test(password))) {alertSuccess(passwordElement)}
+    if (passwordElement.value === passConfirm.value) {alertSuccess(passConfirm)}
+    else {alertError(passwordElement, "Must enter valid password")}
+}
 
 const validateEmailInput = () => {
     const emailValue = email.value
     if (emailValue === '') {alertError(email, 'Email is required')}
     if (!isValidEmail(emailValue)) {alertError(email, 'Must provide valid email')}
     else {alertSuccess(email)}
-    //
-    // const zipValue = zip.value
-    // if (/^([0-9]{5})(?:[-\s]*([0-9]{4}))?$/.test(zipValue)) {alertSuccess(email)}
-    // else {alertError(zip, "Must provide valid US zip code.  Example XXXXX or XXXXX-XXXX")}
 }
+
 const validateZipInput = () => {
     const zipValue = zip.value
     if (zipValue === '') {alertError(zip, "Zip Code is required")}
-    if (!isValidZip(zipValue)) {alertError(zip, "Must provide valid zip code")}
+    if (!isValidZip(zipValue)) {alertError(zip, "Must provide valid zip code (XXXXX or XXXXX-XXXX)")}
+    else {alertSuccess(zip)}
 }
+
+// const validatePassword = () => {
+//     const passValue = password.value
+//     if (passValue === '') {alertError(password, "Password is required")}
+//     if (!isValidPassword(passValue)) {alertError(password, "Must contain more than 8 digits and 1 number")}
+//     else {alertSuccess(password)}
+// }
+
+// function isValidPassword(password) {
+//     if ((password.length > 8) && (/\d/.test(password))) {}
+// }
 
 function isValidEmail(email) {
     return /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
     .test(email)
 }
-
-function isValidPostalCode(postalCode, countryCode) {
-    switch (countryCode) {
-        case "US":
-            postalCodeRegex = /^([0-9]{5})(?:[-\s]*([0-9]{4}))?$/;
-            break;
-        case "CA":
-            postalCodeRegex = /^([A-Z][0-9][A-Z])\s*([0-9][A-Z][0-9])$/;
-            break;
-        default:
-            postalCodeRegex = /^(?:[A-Z0-9]+([- ]?[A-Z0-9]+)*)?$/;
-    }
-    return console.log(postalCodeRegex.test(postalCode));
+function isValidZip(zip) {
+    return (/^([0-9]{5})(?:[-\s]*([0-9]{4}))?$/).test(zip)
 }
 
 const alertSuccess = (element) => {
@@ -59,3 +78,17 @@ const alertError = (element, message) => {
     formElement.classList.add('error')
     formElement.classList.remove('success')
 }
+
+// function isValidPostalCode(postalCode, countryCode) {
+//     switch (countryCode) {
+//         case "US":
+//             postalCodeRegex = /^([0-9]{5})(?:[-\s]*([0-9]{4}))?$/;
+//             break;
+//         case "CA":
+//             postalCodeRegex = /^([A-Z][0-9][A-Z])\s*([0-9][A-Z][0-9])$/;
+//             break;
+//         default:
+//             postalCodeRegex = /^(?:[A-Z0-9]+([- ]?[A-Z0-9]+)*)?$/;
+//     }
+//     return console.log(postalCodeRegex.test(postalCode));
+// }
